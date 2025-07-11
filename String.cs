90e7375e0a834e9f3e468915c862c7570c6d410c
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 17:07:44 +0530

    Create String.cs

---
+++ String.cs
@@ -0,0 +1,12 @@
+//String program
+using System;
+class Derived
+{
+    public static void Main(String [] args)
+    {
+        String Name;
+        Console.WriteLine("\nEnter your name....");
+        Name=Console.ReadLine();
+        Console.WriteLine("\nHello..."+(Name));
+    }
+}
