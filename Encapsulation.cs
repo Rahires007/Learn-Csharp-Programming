df5dbc37e2257a0d3d975e57e66d1e3585f758b0
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 17:37:44 +0530

    Update Encapsulation.cs

--- Encapsulation.cs
+++ Encapsulation.cs
@@ -1,1 +1,22 @@
+// Class and Object program using Encapsulation
+using System;
+
+class Encapsulation
+{
+  
+    public void Square(int x)
+    {
+        Console.WriteLine("\nSquare = " + (x * x));
+    }
+
+   
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int Num = Convert.ToInt32(Console.ReadLine());
+        
+        Encapsulation Encap = new Encapsulation();
+        Encap.Square(Num);
+    }
+}
 
