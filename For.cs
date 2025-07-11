013d4d4578937901a8df057a9495a9421a6d9e71
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 17:00:28 +0530

    Create For.cs

---
+++ For.cs
@@ -0,0 +1,12 @@
+//For loop program
+using System;
+class For
+{
+    public static void Main(String [] args)
+    {
+        for(int Num=1; Num<=10; Num++)
+        {
+            Console.WriteLine("\n"+(Num));
+        }
+    }
+}
