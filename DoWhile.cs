8d90463461d152669ee19251f021d3688a5ba97c
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 16:59:55 +0530

    Create DoWhile.cs

---
+++ DoWhile.cs
@@ -0,0 +1,15 @@
+//Do while loop program
+using System;
+class DoWhile
+{
+    public static void Main(String [] args)
+    {
+        int Num=1;
+        do
+        {
+            Console.WriteLine("\n"+(Num));
+            Num++;
+        }
+        while(Num<=10);
+    }
+}
