577668ddffa03c95fc9a11ebdb97fc28d1b31aca
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 17:08:32 +0530

    Create While.cs

---
+++ While.cs
@@ -0,0 +1,14 @@
+//While loop program
+using System;
+class While
+{
+    public static void Main(String [] args)
+    {
+        int Num=1;
+        while(Num<=10)
+        {
+            Console.WriteLine("\n"+(Num));
+            Num++;
+        }
+    }
+}
