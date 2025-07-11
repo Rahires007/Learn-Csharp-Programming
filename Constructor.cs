3f768994f8140fc92774093b9ff108e61de00376
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 18:32:39 +0530

    Create Constructor.cs

---
+++ Constructor.cs
@@ -0,0 +1,13 @@
+//Constructor program
+using System;
+class Sample
+{
+	Sample()
+	{
+		Console.WriteLine("\nHello World....");
+	}
+	public static void Main(String [] args)
+	{
+		Sample S=new Sample();
+	}
+}
